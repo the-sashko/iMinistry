dca9a44d124076e9c2765adf0309f4d251dad3b2
The Sashko <inbox@sashko.me>
2019-10-13 23:46:33 +0300

    fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,7 +9,7 @@ mkdir bin
 mkdir data
 
 cp -r install/config data/config
-ln config/sites.json data/config/sites.json
+ln -s config/sites.json data/config/sites.json
 
 chmod -R 755 data
 chmod -R 755 bin
